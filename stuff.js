bc91db8f92e1e66f315347943a3c620766c1e025
Vincent Liok <vliok@stuy.edu>
2017-02-28 22:18:44 -0500

    I tried

--- stuff.js
+++ stuff.js
@@ -1,11 +1,33 @@
 var s = document.getElementById("s");
-s.addEventListener("click",connect);
+
+var firstClick = true;
 
 var connect = function(e) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+
+    //circle
     var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
     s.appendChild(c);
-    c.setAttribute("cx","250");
-    c.setAttribute("cy","250");
-    c.setAttribute("r","100");
+    c.setAttribute("cx",x);
+    c.setAttribute("cy",y);
+    c.setAttribute("r","50");
     c.setAttribute("fill","red");
-}
+
+    console.log(firstClick);
+    //line
+    if (firstClick = false) {
+	d = d + "L" + x.toString() + " " + y.toString();
+	p.setAttribute("d",d);
+    } else {
+	var p = document.createElementNS("http://www.w3.org/2000/svg","path");
+	s.appendChild(p);
+	p.setAttribute("stroke","black");
+	var d = "M" + x.toString() + " " + y.toString();
+	p.setAttribute("d",d);
+	firstClick = false;
+    };
+            
+};
+
+s.addEventListener("click",connect);
